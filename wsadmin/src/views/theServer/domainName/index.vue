5fe1b854cef3402a48389d718696f13dfd6ca3fb
2644345145@qq.com <065998lcr>
2025-06-17 19:29:09 +0800

    【前端】代码提交

--- wsadmin/src/views/theServer/domainName/index.vue
+++ wsadmin/src/views/theServer/domainName/index.vue
@@ -92,7 +92,7 @@
         </u-table-column>
         <u-table-column label="到期时间" min-width="100" prop="expiration_time" show-overflow-tooltip>
           <template slot-scope="scope">
-            {{ formatTimestamp(scope.row[scope.column.property]) }}
+            {{ scope.row[scope.column.property] }}
           </template>
         </u-table-column>
         <u-table-column label="失效原因" min-width="120" prop="reason">
